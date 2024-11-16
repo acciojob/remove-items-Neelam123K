bbca19ef8aea093fbb01057a1242398576f37070
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2024-11-16 14:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+        if (selectedOption) {
+                colorSelect.remove(colorSelect.selectedIndex);
+            }
+        }
+	
